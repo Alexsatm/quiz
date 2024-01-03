e53bd763ce76f961d4d455703b004fcefeb30150
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-03 15:30:52 +0300

    add: array question js

---
+++ app.js
@@ -0,0 +1,42 @@
+const questions = [
+    // 1
+    {
+        question: 'Самая большая страна по территории в мире?',
+        answers: [
+            {text: 'Канада', correct: false},
+            {text: 'Россия', correct: true},
+            {text: 'США', correct: false},
+            {text: 'Китай', correct: false},
+        ]
+    },
+    // 2
+    {
+        question: 'сколько всего в мире океанов?',
+        answers: [
+            {text: '4', correct: false},
+            {text: '6', correct: false},
+            {text: '5', correct: true},
+            {text: '7', correct: false},
+        ]
+    },
+    // 3
+    {
+        question: 'Самый большой материк?',
+        answers: [
+            {text: 'Евразия', correct: true},
+            {text: 'Австралия', correct: false},
+            {text: 'Африка', correct: false},
+            {text: 'Антарктида', correct: false},
+        ]
+    },
+    //4
+    {
+        question: 'Самое быстрое животное?',
+        answers: [
+            {text: 'Леопард', correct: false},
+            {text: 'Гепард', correct: true},
+            {text: 'Тигр', correct: false},
+            {text: 'Лев', correct: false},
+        ]
+    }
+];
